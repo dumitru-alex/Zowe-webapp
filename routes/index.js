da36697117cc6a4db0e61ee3b267d8da281b3f6c
Alexandru-Paul Dumitru <ad670553@broadcom.net>
2019-10-24 17:24:03 +0200

    adding a button witch returns mocked data

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/iplinfo', function(req, res, next) {
+  // function to return IPL INFO will be called here
+  let iplinfo = "placeholder value"
+  res.render('index', { title: 'Express' , iplinfo});
+})
+
 module.exports = router;
